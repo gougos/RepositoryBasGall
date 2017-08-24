5b46794b121de6c3eb2af7b39501aa88514fc853
'gougos' <'mohamedhedi.guellali@gmail.com'>
2017-08-24 13:09:00 +0100

    Commit ConfCall
    Signed-off-by: 'gougos' <'mohamedhedi.guellali@gmail.com'>

--- JavaGitHelloWorld/src/com/aiventu/Main.java
+++ JavaGitHelloWorld/src/com/aiventu/Main.java
@@ -7,6 +7,7 @@ public class Main {
          System.out.println("Hello World");
          System.out.println("Deuxiéme ligne 2");
          System.out.println("Deuxiéme ligne 3");
+         System.out.println("Hello");
 	}
 
 }
